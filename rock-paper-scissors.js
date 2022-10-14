179bb594703d07fa6b2a8e0b0e8a9fb86a0ba312
Lux <web-lux@outlook.fr>
2022-10-14 19:13:11 +0200

    added function to get computer choice

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,9 +1,8 @@
-function getRandom() {
-    return Math.floor(Math.random() * 3 + 1)
-}
-
-console.log(getRandom())
-
 function getComputerChoice() {
-
+    const possibleChoices = ["Paper", "Rock", "Scissors"]
+    const randomNumber = Math.floor(Math.random() * 3)
+    return possibleChoices[randomNumber]
 }
+
+const computerSelection = getComputerChoice();
+console.log(computerSelection)
