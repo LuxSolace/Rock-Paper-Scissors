819708b9b2ab4fcd75f876fbad5ad2dbd041779e
Lux <web-lux@outlook.fr>
2022-10-14 21:06:56 +0200

    added dummy player choice and round simulation

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,8 +1,18 @@
 function getComputerChoice() {
-    const possibleChoices = ["Paper", "Rock", "Scissors"]
+    const possibleChoices = ["PAPER", "ROCK", "SCISSORS"]
     const randomNumber = Math.floor(Math.random() * 3)
     return possibleChoices[randomNumber]
 }
 
 const computerSelection = getComputerChoice();
-console.log(computerSelection)
+const playerSelection = "Paper".toUpperCase()
+
+if ((playerSelection === "PAPER" && computerSelection === "ROCK") || 
+    (playerSelection === "ROCK" && computerSelection === "SCISSORS") ||
+    (playerSelection === "SCISSORS" && computerSelection === "PAPER")) {
+        console.log("Player wins !")
+} else if (playerSelection === computerSelection) {
+    console.log("It's a tie !")
+} else {
+    console.log("Computer wins !")
+}
