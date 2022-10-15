ffaf9d7005d8e3838d7e1235c6b850c0c2324889
Lux <web-lux@outlook.fr>
2022-10-15 19:45:47 +0200

    added score track and the possibility of a winner which warranted some changes in the way the playRound() function worked

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,3 +1,7 @@
+let computerChoice
+let playerChoice
+let roundResult
+
 function getComputerChoice() {
     const possibleChoices = ["paper", "rock", "scissors"];
     const randomNumber = Math.floor(Math.random() * 3);
@@ -12,30 +16,54 @@ function capitalize(string) {
     return string[0].toUpperCase() + string.substring((1));
 }
 
-let computerChoice
-let playerChoice
-
 function playRound(playerSelection, computerSelection) {
 
     if ((playerSelection === "paper" && computerSelection === "rock") || 
     (playerSelection === "rock" && computerSelection === "scissors") ||
     (playerSelection === "scissors" && computerSelection === "paper")) {
-        return `You win ! ${capitalize(playerSelection)} beats ${computerSelection}.`
+        return roundResult = "player won"
     } else if (playerSelection === computerSelection) {
-        return `It's a tie !`
+        return roundResult = "tie"
     } else {
-        return `You lose ! ${capitalize(computerSelection)} beats ${playerSelection}. `
+        return roundResult = "player lost"
     }
  
 }
 
+function checkWinner(player, computer) {
+    if (player > computer) {
+        console.log("Player won !!! :)")
+    } else if (player < computer) {
+        console.log("Computer won. ):")
+    } else {
+        console.log("It's a tie. :/")
+    }
+}
+
 function game() {
 
+    let playerScore = 0
+    let computerScore = 0
+
     for (let i = 0; i < 5; i++) {
+
         computerChoice = getComputerChoice();
         playerChoice = getPlayerChoice();
-        console.log(playRound(computerChoice, playerChoice))
+        playRound(playerChoice, computerChoice)
+
+        if (roundResult === "player won") {
+            playerScore ++
+            console.log(`You win ! ${capitalize(playerChoice)} beats ${computerChoice}. Player score : ${playerScore} / Computer score : ${computerScore}`)
+        } else if (roundResult === "tie") {
+            console.log(`It's a tie ! Player score : ${playerScore} / Computer score : ${computerScore}`)
+        } else {
+            computerScore ++
+            console.log(`You lose ! ${capitalize(computerChoice)} beats ${playerChoice}. Player score : ${playerScore} / Computer score : ${computerScore}`)
+        }
+
     }
+
+    checkWinner(playerScore, computerScore)
 }
 
 game()
