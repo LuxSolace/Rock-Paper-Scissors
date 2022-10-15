2ca00fe2954bb2be379891752753ec8ee3202c29
Lux <web-lux@outlook.fr>
2022-10-15 15:23:20 +0200

    added capitalize function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,18 +1,63 @@
 function getComputerChoice() {
-    const possibleChoices = ["PAPER", "ROCK", "SCISSORS"]
+    const possibleChoices = ["paper", "rock", "scissors"]
     const randomNumber = Math.floor(Math.random() * 3)
     return possibleChoices[randomNumber]
 }
 
-const computerSelection = getComputerChoice();
-const playerSelection = "Paper".toUpperCase()
-
-if ((playerSelection === "PAPER" && computerSelection === "ROCK") || 
-    (playerSelection === "ROCK" && computerSelection === "SCISSORS") ||
-    (playerSelection === "SCISSORS" && computerSelection === "PAPER")) {
-        console.log("Player wins !")
-} else if (playerSelection === computerSelection) {
-    console.log("It's a tie !")
-} else {
-    console.log("Computer wins !")
+function capitalize(string) {
+    return string[0].toUpperCase() + string.substring((1))
 }
+
+let computerChoice = getComputerChoice();
+let playerChoice = "Paper".toLowerCase();
+
+console.log(computerChoice)
+console.log(playerChoice)
+
+function playRound(playerSelection, computerSelection) {
+
+    if ((playerSelection === "paper" && computerSelection === "rock") || 
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "scissors" && computerSelection === "paper")) {
+        return `You win ! ${capitalize(playerSelection)} beats ${computerSelection}.`
+    } else if (playerSelection === computerSelection) {
+        return `It's a tie !`
+    } else {
+        return `You lose ! ${capitalize(computerSelection)} beats ${playerSelection}. `
+    }
+
+}
+
+console.log(playRound(playerChoice, computerChoice))
+
+/* const playerSelection = "Paper".toUpperCase()
+
+function playRound(playerSelection, computerSelection) {
+    const computerSelection = getComputerChoice();
+    let playerScore = 0;
+    let computerScore = 0;
+    let result;
+
+    if ((playerSelection === "PAPER" && computerSelection === "ROCK") || 
+        (playerSelection === "ROCK" && computerSelection === "SCISSORS") ||
+        (playerSelection === "SCISSORS" && computerSelection === "PAPER")) {
+        playerScore ++;
+        result = `You win ! ${playerSelection} beats ${computerSelection}`
+        return 
+    } else if (playerSelection === computerSelection) {
+        result = `It's a tie !`
+    } else {
+        computerScore ++;
+        result = `You lose ! ${computerSelection} beats ${playerSelection} `
+        return
+    }
+}
+
+/* function game() {
+    for (let i = 0; i < 5; i++) {
+        playRound(playerSelection, computerSelection)
+        console.log(playerScore)
+        console.log(computerScore)
+        console.log(result)
+    }
+} */
