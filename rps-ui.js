8034d63e8cb6dc8135fd9f896643aabaa8b7ebe8
Lux <web-lux@outlook.fr>
2022-10-19 20:55:32 +0200

    refractored some of the function and added animations

--- rps-ui.js
+++ rps-ui.js
@@ -1,15 +1,20 @@
 const villainHealthBar = document.getElementById("villain-health-bar")
 const heroHealthBar = document.getElementById("hero-health-bar")
 
+const heroContainer = document.getElementById("hero-container")
+const villainContainer = document.getElementById("villain-container")
+
 const villainSprite = document.getElementById("villain-img")
 const heroSprite = document.getElementById("hero-img")
 
-const villainAction = document.getElementById("villain-choice")
-const heroAction = document.getElementById("hero-choice")
+const villainActionImg = document.getElementById("villain-choice")
+const heroActionImg = document.getElementById("hero-choice")
 
 const restartBtn = document.getElementById("restart-btn")
 const roundTextContainer = document.getElementById("round-result")
 const playerPrompt = document.getElementById("player-prompt")
+
+const weaponsBtn = document.querySelectorAll("input.weapons-btn")
 
 const sword = "./images/sword.jpg"
 const crossbow = "./images/crossbow.jpg"
@@ -17,56 +22,56 @@ const axe = "./images/axe.jpg"
 
 let roundResult
 
+const intervalTextAppeareance = 1800
+const intervalButtonDisabled = 2000
+const intervalDamageAnimation = 1000
+const intervalHealthBarLoss = 1000
+const intervalDeathAnimation = 1000
+const intervalPlayAgainMessage = 2000
+
+
 function actor(hp, dom, choice) {
     this.hp = hp,
     this.dom = dom,
     this.choice = choice
 }
 
-/* dom initialized */
+function weaponStringToJS(string) {
+    if (string === "sword") {
+        return sword
+    } else if (string === "crossbow") {
+        return crossbow
+    } else if (string === "axe") {
+        return axe
+    }
+}
 
 const hero = new actor(5, heroHealthBar)
 const villain = new actor(5, villainHealthBar)
 
-function playDamageAnimation(sprite) {
-    setTimeout(function() {
-    sprite.classList.add("damaged")
-    void sprite.offsetWidth;
-    setTimeout(function() {
-        sprite.classList.remove("damaged") },
-        1000)
-    }, 1000)
+function game(playerChoice) {
+    villain.choice = getComputerChoice();
+    hero.choice = playerChoice;
+    playChoiceAnimation(villainActionImg, weaponStringToJS(villain.choice))
+    playChoiceAnimation(heroActionImg, weaponStringToJS(hero.choice))
+    compareChoice(hero.choice, villain.choice);
+    playRound(roundResult);
+    checkWinner()
 }
-
-function playChoiceAnimation(actor, weapon) {
-    actor.src = weapon
-    actor.classList.add("weapon-chosen")
-    void actor.offsetWidth;
-    setTimeout(function() {
-        actor.classList.remove("weapon-chosen") },
-        2000)
-}
-
-function announceText(string) {
-    setTimeout(function() {
-        roundTextContainer.style.opacity = 1
-        roundTextContainer.textContent = string
-    }, 1800)
-}
-
-/* function playTextAnimation(text) {
-    roundTextContainer.textContent = text
-    roundTextContainer.classList.add("text-appeared")
-    void roundTextContainer
-    setTimeout(function() {
-        roundTextContainer.classList.remove("text-appeared") },
-        3000)
-} */
 
 function getComputerChoice() {
     const possibleChoices = ["sword", "crossbow", "axe"];
     const randomNumber = Math.floor(Math.random() * 3);
     return possibleChoices[randomNumber];
+}
+
+function playChoiceAnimation(actorActionImg, weapon) {
+    actorActionImg.src = weapon
+    actorActionImg.classList.add("weapon-chosen")
+    void actorActionImg.offsetWidth;
+    setTimeout(() => {
+        actorActionImg.classList.remove("weapon-chosen") },
+        2000)
 }
 
 function compareChoice(playerSelection, computerSelection) {
@@ -81,90 +86,109 @@ function compareChoice(playerSelection, computerSelection) {
     }
 }
 
-function modifyHealthBar(loser) {
-    setTimeout(function() {
-        loser.dom.style.width = (20 * loser.hp) + "%"
-    }, 1000)
-}
-
 function playRound(victoryState) {
-    disableWeaponsBtn(2000)
+    disableWeaponsBtn()
     if (victoryState === "playerVictory") {
         villain.hp --
         announceText("Hero strikes !")
-        if (villain.hp != 0) {playDamageAnimation(villainSprite)}
+        villain.hp != 0 && playDamageAnimation(villainSprite)
         modifyHealthBar(villain)
     } else if (victoryState === "playerLoss") {
         hero.hp --
         announceText("Ennemy strikes !")
-        if (hero.hp !=0) {playDamageAnimation(heroSprite)}
+        hero.hp !=0 && playDamageAnimation(heroSprite)
         modifyHealthBar(hero)
     } else {
         announceText("It's a tie !")
     }
 }
 
-function disableWeaponsBtn(interval) {
-    document.querySelectorAll("input.weapons-btn").forEach(function(input) {
-        input.disabled = true;
-        input.classList.add("greyed-out"); })
+function disableWeaponsBtn() {
 
-    setTimeout(function() {
-    document.querySelectorAll("input.weapons-btn").forEach(function(input) {
+    weaponsBtn.forEach((input) => {
+        input.disabled = true;
+        input.classList.add("greyed-out") })
+
+    setTimeout(() => {
+        weaponsBtn.forEach((input) => {
         input.disabled = false;
-        input.classList.remove("greyed-out")
-    }) }, interval)
+        input.classList.remove("greyed-out") } ) 
+    }, intervalButtonDisabled)
 }
 
-function playDeathAnimation(sprite) {
-    setTimeout(function() {
-        sprite.classList.add("dead")
+function announceText(string) {
+    setTimeout(() => {
+        roundTextContainer.style.opacity = 1
+        roundTextContainer.textContent = string
+    }, intervalTextAppeareance)
+}
+
+function playDamageAnimation(sprite) {
+    setTimeout(() => {
+        sprite.classList.add("damaged")
         void sprite.offsetWidth;
-    }, 1000)
+        setTimeout(() => {
+            sprite.classList.remove("damaged") },
+            intervalDamageAnimation)
+    }, intervalDamageAnimation)
+}
+
+function modifyHealthBar(loser) {
+    setTimeout(() => {
+        loser.dom.style.width = (20 * loser.hp) + "%"
+    }, intervalHealthBarLoss)
 }
 
 function checkWinner() {
+
     if (villain.hp === 0 || hero.hp === 0) {
 
-        setTimeout(function() {
-            document.querySelectorAll("input.weapons-btn").forEach(function(input) {
+        setTimeout(() => {
+            weaponsBtn.forEach((input) => {
             input.disabled = true
             input.classList.add("greyed-out")
-        })}, 2001)
+        })}, (intervalButtonDisabled + 1))
 
         if (villain.hp === 0) {
-            playDeathAnimation(villainSprite)
+            playDeathAnimation(villainContainer)
         } else if (hero.hp === 0) {
-            playDeathAnimation(heroSprite)
+            playDeathAnimation(heroContainer)
         }
 
-        setTimeout(function() {
+        setTimeout(() => {
             playerPrompt.textContent = "play again ?"
             playerPrompt.style.cursor = "pointer"
             playerPrompt.addEventListener("click", resetGame)
-        }, 2000)
+        }, intervalPlayAgainMessage)
+
+        setTimeout(() => {
+            roundTextContainer.classList.add("fade-out")
+            void roundTextContainer.offsetWidth
+        }, 5000)
     }
 }
 
-function game(playerChoice) {
-    villain.choice = getComputerChoice();
-    hero.choice = playerChoice;
-    playChoiceAnimation(villainAction, eval(villain.choice))
-    playChoiceAnimation(heroAction, eval(hero.choice))
-    compareChoice(hero.choice, villain.choice);
-    playRound(roundResult);
-    checkWinner()
+function playDeathAnimation(sprite) {
+    setTimeout(() => {
+        sprite.classList.add("dead")
+        void sprite.offsetWidth;
+    }, intervalDeathAnimation)
 }
 
 function resetGame() {
-    playerPrompt.removeEventListener("click", resetGame)
-    document.querySelectorAll("input.weapons-btn").forEach(input => input.disabled = false);
     villain.hp = 5;
     hero.hp = 5;
+    playerPrompt.removeEventListener("click", resetGame)
     modifyHealthBar(villain)
     modifyHealthBar(hero)
+    weaponsBtn.forEach((input) => {
+        input.disabled = false
+        input.classList.remove("greyed-out")
+    });
     playerPrompt.textContent = "what will you do ?"
     playerPrompt.style.cursor = ""
-    villainSprite.classList.remove("dead")
-    heroSprite.classList.remove("dead")
+    villainContainer.classList.remove("dead")
+    heroContainer.classList.remove("dead")
+    roundTextContainer.classList.remove("fade-out")
+    roundTextContainer.style.opacity = 0
 }
